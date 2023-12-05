eb4fb64bc1e952405a82ee47406cd7acbe60ed9b
anna waltrip <annawaltrip@gmail.com>
2023-12-05 19:50:59 +0000

    Restaurant challenge 11

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,13 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+UPDATE Customers
+SET FavoriteDish=(SELECT DishID FROM Dishes
+WHERE Name='Quinoa Salmon Salad')
+WHERE CustomerID=(SELECT CustomerID FROM Customers
+WHERE FirstName='Cleo' AND LastName='Goldwater');
+
+SELECT Customers.FirstName, Customers.LastName, Dishes.Name
+FROM Customers
+JOIN Dishes ON Customers.FavoriteDish = Dishes.DishID;
