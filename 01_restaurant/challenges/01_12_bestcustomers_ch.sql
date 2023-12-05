7c1a656a33805d86075c15f6ba4fe3fb07a965f7
anna waltrip <annawaltrip@gmail.com>
2023-12-05 21:21:47 +0000

    Restaurant challenge 12

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,10 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+SELECT COUNT(Orders.OrderID), Customers.FirstName, Customers.LastName
+FROM Orders
+JOIN Customers ON Orders.OrderID = Customers.CustomerID
+GROUP BY Orders.CustomerID
+HAVING COUNT(Orders.OrderID) >= 3
+ORDER BY COUNT(Orders.OrderID) DESC;
