a6f9841200f28fd0ed7b35ecea0bdfcf87165690
anna waltrip <annawaltrip@gmail.com>
2023-12-05 21:25:05 +0000

    Restaurant challenge 12 update

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -2,9 +2,9 @@
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
 
-SELECT COUNT(Orders.OrderID), Customers.FirstName, Customers.LastName
+SELECT COUNT(Orders.OrderID) AS OrderCount, Customers.FirstName, Customers.LastName
 FROM Orders
 JOIN Customers ON Orders.OrderID = Customers.CustomerID
 GROUP BY Orders.CustomerID
-HAVING COUNT(Orders.OrderID) >= 3
-ORDER BY COUNT(Orders.OrderID) DESC;
+HAVING OrderCount >= 3
+ORDER BY OrderCount DESC;
